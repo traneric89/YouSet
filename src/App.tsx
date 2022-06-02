435303d5e6706af29d432e685ebf3a0820d9e4e6
Ariane Tourangeau <atourangeau@allset.ca>
2022-06-02 14:40:15 -0400

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, ThemeProvider, Toolbar, Typography } from "@material-ui/core";
+import { theme } from "./theme";
 
-function App() {
+export const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography>Technical Assignment</Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
